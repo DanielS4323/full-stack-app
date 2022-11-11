bb4392dbc708eddc7801354d6b73e0702966929b
DanielS4323 <daky4323@gmail.com>
2022-11-11 16:19:38 +0100

    input change

--- www/src/components/Todo/CreateTodo.js
+++ www/src/components/Todo/CreateTodo.js
@@ -6,11 +6,12 @@ const CreateTodo = () => {
       <div class="col s8 l6 offset-l3 grey darken-4">
         <div className="input-field">
           <i className="material-icons prefix white-text">mode_edit</i>
-          <textarea
-            id="icon_prefix2"
-            className="white-text materialize-textarea"
-          ></textarea>
-          <label htmlFor="icon_prefix2" className="white-text">
+          <input
+            id="icon_prefix"
+            type="text"
+            className="white-text active"
+          ></input>
+          <label htmlFor="icon_prefix" className="white-text">
             Enter To do
           </label>
           <div class="row">
