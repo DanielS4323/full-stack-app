60582f9fe98e07defa8dfeca69aac50d666f73e1
DanielS4323 <daky4323@gmail.com>
2022-11-13 14:42:43 +0100

    ui update

--- www/src/components/Todo/CreateTodo.js
+++ www/src/components/Todo/CreateTodo.js
@@ -3,7 +3,7 @@ import React from "react";
 const CreateTodo = () => {
   return (
     <div className="row">
-      <div class="col s8 l6 offset-l3 grey darken-4">
+      <div className="col s8 l6 offset-l3 grey darken-4">
         <div className="input-field">
           <i className="material-icons prefix white-text">mode_edit</i>
           <input
@@ -14,15 +14,15 @@ const CreateTodo = () => {
           <label htmlFor="icon_prefix" className="white-text">
             Enter To do
           </label>
-          <div class="row">
-            <div class="col-12 center">
+          <div className="row">
+            <div className="col-12 center">
               <button
                 className="deep-purple lighten-1 white-text btn waves-effect waves-light margin top-10 brown-text hoverable"
                 type="submit"
                 name="action"
               >
                 Add Todo
-                <i class="material-icons right">note_add</i>
+                <i className="material-icons right">note_add</i>
               </button>
             </div>
           </div>
