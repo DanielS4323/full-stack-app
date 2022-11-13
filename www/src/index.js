dcecf07aa71ee2d55720aa2ffdbb32f91e0f0e2d
DanielS4323 <daky4323@gmail.com>
2022-11-13 18:57:58 +0100

    provider add

--- www/src/index.js
+++ www/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'materialize-css/dist/css/materialize.min.css'
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "materialize-css/dist/css/materialize.min.css";
+import reportWebVitals from "./reportWebVitals";
+import TodoProvider from "./store/TodoProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </React.StrictMode>
 );
 
