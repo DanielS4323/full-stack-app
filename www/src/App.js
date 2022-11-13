eedddbc8637e1efb9722c47a1dd3eb6d0330f1d5
DanielS4323 <daky4323@gmail.com>
2022-11-13 18:56:10 +0100

    conditional display

--- www/src/App.js
+++ www/src/App.js
@@ -2,14 +2,20 @@ import Header from "./components/UI/Header";
 import CreateTodo from "./components/Todo/CreateTodo";
 import TodoList from "./components/Todo/TodoList";
 import "./index.css";
+import { useContext } from "react";
+import TodoContext from "./store/Todo-Context";
+import EditTodo from "./components/Todo/EditTodo";
 
 function App() {
+  const { todoToEdit } = useContext(TodoContext);
+  // console.log(todoToEdit);
   return (
     <div className="App">
       <Header />
       <div className="row margin top-100">
         <div className="container">
-          <CreateTodo />
+          {!todoToEdit && <CreateTodo />}
+          {todoToEdit && <EditTodo />}
         </div>
         <div className="col s8 l6 offset-l3">
           <TodoList />
