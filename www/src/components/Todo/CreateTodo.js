9bab1a56742fc3bdff974310db2e287f7a14d03b
DanielS4323 <daky4323@gmail.com>
2022-11-13 18:58:46 +0100

    handle todo

--- www/src/components/Todo/CreateTodo.js
+++ www/src/components/Todo/CreateTodo.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
+import TodoContext from "../../store/Todo-Context";
 
 const CreateTodo = () => {
+  const { addNewTodo } = useContext(TodoContext);
+  const todoText = useRef();
+
+  const addNewTodoHandler = () => {
+    let text = todoText.current.value.trim();
+    let completed = false;
+
+    if (text.length > 1) {
+      addNewTodo(text, completed);
+
+      todoText.current.value = "";
+    }
+    return;
+  };
+
   return (
     <div className="row">
       <div className="col s8 l6 offset-l3 grey darken-4">
@@ -10,6 +26,7 @@ const CreateTodo = () => {
             id="icon_prefix"
             type="text"
             className="white-text active"
+            ref={todoText}
           ></input>
           <label htmlFor="icon_prefix" className="white-text">
             Enter To do
@@ -20,6 +37,7 @@ const CreateTodo = () => {
                 className="deep-purple lighten-1 white-text btn waves-effect waves-light margin top-10 brown-text hoverable"
                 type="submit"
                 name="action"
+                onClick={addNewTodoHandler}
               >
                 Add Todo
                 <i className="material-icons right">note_add</i>
