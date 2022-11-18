c8991835c72932303d1379c5ceb561f4285cfe95
DanielS4323 <daky4323@gmail.com>
2022-11-18 16:02:48 +0100

    delete , mark , edit

--- www/src/service.js
+++ www/src/service.js
@@ -16,12 +16,59 @@ export const getAllTodos = async () => {
 export const deleteTodo = async (id) => {
   const response = await fetch(`${URL}/${TODOS}/${id}`, {
     method: "DELETE",
-  
   });
   const data = await response.json();
   if (!response.ok) {
-    throw new Error(data.message || "Could not delete article");
+    throw new Error("Could not delete todo.");
   }
-  
-  return null
+
+  return null;
+};
+
+export const addNewTodo = async (newTodo) => {
+  const response = await fetch(`${URL}/${TODOS}`, {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Could not add new todo.");
+  }
+  return data;
+};
+
+export const markTodo = async (id, completed) => {
+  const response = await fetch(`${URL}/${TODOS}/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(completed),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Could not mark todo.");
+  }
+  return data;
+};
+
+export const editTodo = async (id, text) => {
+  const response = await fetch(`${URL}/${TODOS}/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(text),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Could not mark todo.");
+  }
+  return data;
 };
