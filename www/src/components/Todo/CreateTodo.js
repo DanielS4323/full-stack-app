ca1b91033b6b1016bf25bcdd0d19588f9efd36b7
DanielS4323 <daky4323@gmail.com>
2022-11-19 16:56:59 +0100

    var name changed

--- www/src/components/Todo/CreateTodo.js
+++ www/src/components/Todo/CreateTodo.js
@@ -4,30 +4,31 @@ import Button from "../UI/Button";
 
 const CreateTodo = () => {
   const { addNewTodo } = useContext(TodoContext);
-  const text = useRef("");
+  const textInputRef = useRef("");
 
   const addNewTodoHandler = () => {
-    let todoText = text.current.value.trim();
-    let completed = '';
+    let todoText = textInputRef.current.value.trim();
+    let completed = "";
 
     if (todoText.length > 1) {
       addNewTodo(todoText, completed);
-      text.current.value = "";
+      textInputRef.current.value = "";
     }
     return;
   };
 
   return (
     <div className="row">
-      <div className="col s8 l6 offset-l3 grey darken-4">
+      <div className="col s8 l6 offset-l3 grey darken-4  z-depth-">
         <div className="input-field">
           <i className="material-icons prefix white-text">mode_edit</i>
           <input
             id="icon_prefix"
             type="text"
             className="white-text active"
-            ref={text}
-          ></input>
+            ref={textInputRef}
+            autoFocus
+          />
           <label htmlFor="icon_prefix" className="white-text">
             Enter To do
           </label>
