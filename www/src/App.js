3fac98585dc19114879132f0be3f33c06dc6efdf
DanielS4323 <daky4323@gmail.com>
2022-12-29 15:30:18 +0100

    formated

--- www/src/App.js
+++ www/src/App.js
@@ -7,25 +7,20 @@ import TodoContext from "./store/Todo-Context";
 import EditTodo from "./components/Todo/EditTodo";
 import Layout from "./components/layout/Layout";
 
-
 function App() {
   const { todoToEdit } = useContext(TodoContext);
 
-  
-
   return (
     <Layout>
-     
-        <div className="row margin top-100">
-          <div className="container">
-            {!todoToEdit && <CreateTodo />}
-            {todoToEdit && <EditTodo />}
-          </div>
-          <div className="col s8 l6 offset-l3">
-            <TodoList />
-          </div>
+      <div className="row margin top-100">
+        <div className="container">
+          {!todoToEdit && <CreateTodo />}
+          {todoToEdit && <EditTodo />}
         </div>
-     
+        <div className="col s8 l6 offset-l3">
+          <TodoList />
+        </div>
+      </div>
     </Layout>
   );
 }
