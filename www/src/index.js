830d65157780fbcbc82e213152c8cd0511157e8b
DanielS4323 <daky4323@gmail.com>
2022-12-29 15:31:04 +0100

    todoProvier in queryClient

--- www/src/index.js
+++ www/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "materialize-css/dist/css/materialize.min.css";
-import reportWebVitals from "./reportWebVitals";
 import TodoProvider from "./store/TodoProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -11,15 +10,10 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <TodoProvider>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <TodoProvider>
         <App />
-      </QueryClientProvider>
-    </TodoProvider>
+      </TodoProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
