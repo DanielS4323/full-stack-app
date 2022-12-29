62314f42f0bb0da8479c8105ee8824d929c9cb49
DanielS4323 <daky4323@gmail.com>
2022-12-29 15:33:14 +0100

    functions adjusted

--- www/src/store/Todo-Context.js
+++ www/src/store/Todo-Context.js
@@ -1,17 +1,12 @@
 import React from "react";
 
 const TodoContext = React.createContext({
-  isLoading: true,
-  getAllTodos: [],
-  totalTodos: 0,
-  completedTodos: 0,
   todoToEdit: {},
-  addNewTodo: (text, completed) => {},
-  deleteTodo: (id) => {},
-  markTodo: (id) => {},
-  editTodo: (id, text) => {},
-  editToggle: (id) => {},
-  cancelToggle: () => {}
+  addNewTodo: () => {},
+  deleteTodo: () => {},
+  updateTodo: () => {},
+  editToggle: (todo) => {},
+  cancelToggle: () => {},
 });
 
 export default TodoContext;
